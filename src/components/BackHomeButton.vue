59dca5fb3aee95e49b5a74b57deae7903956d648
Domagoj Svjetličić <domagoj.svjetlicic@gmail.com>
2020-03-09 02:02:53 +0100

    deploy changes

---
+++ src/components/BackHomeButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <router-link to="/" class="co-back-home-button">
+        <fa class="co-back-home-button__icon" :icon="['fas', 'arrow-left']"></fa>
+        <small class="u-a5 --uppercase">Back to home</small>
+    </router-link>
+</template>
+
+<script lang="ts">
+    import {Vue, Component} from 'vue-property-decorator';
+
+    @Component({
+        name: 'BackHomeButton',
+    })
+    export default class BackHomeButton extends Vue {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .co-back-home-button {
+        color: $orange;
+
+        &__icon {
+            margin-right: 10px;
+            font-size: 10px;
+        }
+    }
+</style>
---
+++ src/interfaces/ProjectInfo.ts
@@ -0,0 +1,8 @@
+export interface ProjectInfoInterface {
+    routeName: string;
+    title: string;
+    paragraphs: string[];
+    linkInfo?: { link: string, content: string };
+    techs: string[];
+    images: string[];
+}
