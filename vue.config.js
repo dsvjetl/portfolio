6987e866c543d842c25cf18f139981c9165cfa8f
Domagoj Svjetličić <domagoj.svjetlicic@gmail.com>
2020-03-25 14:50:12 +0100

    deploy update

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,14 @@ module.exports = {
                 prependData: `@import "~@/assets/scss/style.scss";`
             }
         }
-    }
+    },
+    chainWebpack: config => {
+        config.performance
+            .maxEntrypointSize(400000)
+            .maxAssetSize(400000)
+    },
+    configureWebpack: {
+        devtool: 'source-map',
+        performance: { hints: false },
+    },
 };
